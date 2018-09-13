c277e8241db2ae8bab29d255eebf692b6cc9240a
rajat pawar <rajat.pawar100@gmail.com>
2018-09-13 17:45:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now} ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile("logs", `${log}\n`, (err) =>{
+		if(err)
+			console.log("unable to write");
+	});
+	next();
+})
+app.use((req, res, next) => {
+	res.render("maintainenace.ejs");
+});
+
+app.get('/', (req, res) => {
+	res.render("home.ejs", {
+		title: "suuuuuup",
+		body: "i'm home",
+		currentDate: new Date().getFullYear()
+	});
+
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: "about",
+		body: "wanna know anytjing bout me",
+		currentDate: new Date().getFullYear()
+
+	});
+});
+app.listen(8080, () => {
+	console.log("server running at port 8080 ...")
+})
